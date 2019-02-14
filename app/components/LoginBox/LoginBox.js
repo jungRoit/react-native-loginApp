69b01300a864c82874720581ae36b85fc9db6994
jungRoit <roit.jung@gmail.com>
2019-02-14 13:41:54 +0545

    login box styles added

--- app/components/LoginBox/LoginBox.js
+++ app/components/LoginBox/LoginBox.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
+
+import styles from './styles';
 
 export default class LoginBox extends React.Component {
   render() {
     return (
       <View>
-        <Text>Login into the App</Text>
-        <TextInput />
-        <TextInput />
+        <Text style={styles.header}>Login into the App</Text>
+        <TextInput style={styles.input} />
+        <TextInput secureTextEntry={true} style={styles.input} />
+        <Button title="Login" />
       </View>
-    )
+    );
   }
 }
---
+++ app/components/LoginBox/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  input: {
+    fontSize: 20,
+    color: 'd3d3d3',
+    backgroundColor: 'black',
+  }
+});
+
+export default styles;
