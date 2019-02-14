1a4c9ae860d10909d52f5bda4487a62261f8b6b9
jungRoit <roit.jung@gmail.com>
2019-02-14 13:33:23 +0545

    dashboard added

--- app/Navigation/StackNavigator.js
+++ app/Navigation/StackNavigator.js
@@ -1,10 +1,14 @@
 import {createStackNavigator, createAppContainer } from 'react-navigation';
 
 import LoginView from '../screens/Login';
+import DashboardView from '../screens/Dashboard';
 
 const StackNavigator = createStackNavigator({
   Login: {
     screen: LoginView,
+  },
+  Dashboard: {
+    screen: DashboardView,
   }
 })
 
---
+++ app/screens/Dashboard/DashboardView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class DashboardView extends React.Component {
+render() {
+return (
+<View>
+  <Text>Dashboard</Text>
+</View>
+);
+}
+}
